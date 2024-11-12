d04c840c741c5d5059c9dff181a74dcd02e4b31e
BaDuc1801 <minhduc180104@gmail.com>
2024-11-12 19:19:22 +0700

    tạo layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import Home from "./components/Home";
 import { Route, Routes, Navigate } from "react-router-dom";
-import ListPosts from "./components/posts&comments/ListPosts.tsx";
 import Auth from "./components/Auth.tsx";
 import Login from "./components/Login.tsx";
 import Register from "./components/Register.tsx";
+import Explore from "./components/Explore.tsx";
+import Home from "./components/Home.tsx";
+import Layout from "./components/Layout.tsx";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="" element={<ListPosts/>} />
+      <Route path="" element={<Layout />}>
+        <Route path="" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+      </Route>
       <Route path="/auth" element={<Auth />}>
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
--- src/components/DestinationCard.tsx
+++ src/components/DestinationCard.tsx
@@ -10,15 +10,8 @@ interface DestinationCardProps {
 
 const DestinationCard: React.FC<DestinationCardProps> = ({ city, name, description, image }) => {
   return (
-    <div className="destination-card p-4 rounded-lg shadow-lg bg-white cursor-pointer">
+    <div className="rounded-lg overflow-hidden shadow-lg bg-white cursor-pointer mx-2 h-32 border-2 border-gray-500">
       <img src={image} alt={name} className="h-48 w-full object-cover rounded-md" />
-      <p className="text-gray-500 mt-2 flex items-center gap-1"><span className="text-red-500"><FaLocationDot />
-      </span>{city}</p>
-      <h3 className="text-xl font-semibold mt-4">{name}</h3>
-      <p className="text-gray-700 mt-2">{description}</p>
-      <div className="flex justify-end">
-      <button className=" bg-red-500 text-white mt-3 pr-2 pl-2 pt-1 pb-1 rounded-lg hover:bg-red-600">Explore</button>
-      </div>
     </div>
   );
 };
