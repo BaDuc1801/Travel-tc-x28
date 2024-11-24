7d237227e948776454e3cfcef7f88c58927aa7b2
BaDuc1801 <minhduc180104@gmail.com>
2024-11-24 18:11:29 +0700

    24/11

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react';
 import PostList from './posts&comments/ListPosts.tsx';
 import DestinationCard from './DestinationCard.tsx';
 import axios from 'axios';
-import { BsStars } from 'react-icons/bs';
+import { BsFilePostFill, BsStars } from 'react-icons/bs';
+import { FaHandHoldingHeart } from 'react-icons/fa6';
+import { BiSolidCommentDetail } from 'react-icons/bi';
+import { RiUserFollowFill } from 'react-icons/ri';
+import { Menu, MenuProps } from 'antd';
 
 const beUrl = import.meta.env.VITE_APP_BE_URL;
 
@@ -15,17 +19,17 @@ interface DestinationCardType {
 }
 
 export interface IUser {
-    id: string; // Tương ứng với _id của MongoDB
+    id: string;
     name: string;
     email: string;
     password: string;
     profilePic: {
         profilePicture: string;
-        bannerImage?: string; // Có thể không có giá trị
+        bannerImage?: string;
     };
-    followers: string[]; // Danh sách ID của người theo dõi
-    following: string[]; // Danh sách ID của người đang theo dõi
-    posts: string[]; // Danh sách ID bài viết
+    followers: string[];
+    following: string[];
+    posts: string[];
     settings: {
         privateAccount: boolean;
         notifications: {
@@ -34,10 +38,61 @@ export interface IUser {
             sms: boolean;
         };
     };
-    createdAt: string; // ISO date string
-    updatedAt: string; // ISO date string
+    createdAt: string;
+    updatedAt: string;
 }
 
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+  {
+    key: 'sub1',
+    label: 'Hồ sơ cá nhân',
+    // icon: <MailOutlined />,
+  },
+  {
+    key: 'sub2',
+    label: 'Bài viết',
+    // icon: <AppstoreOutlined />,
+  },
+  {
+    key: 'sub3',
+    label: 'Người theo dõi',
+    // icon: <SettingOutlined />,
+  },
+  {
+    key: 'sub4',
+    label: 'Thư viện',
+  },
+  {
+    key: 'sub2',
+    label: 'Bài viết',
+    // icon: <AppstoreOutlined />,
+  },
+  {
+    key: 'sub3',
+    label: 'Người theo dõi',
+    // icon: <SettingOutlined />,
+  },
+  {
+    key: 'sub4',
+    label: 'Thư viện',
+  },
+  {
+    key: 'sub2',
+    label: 'Bài viết',
+    // icon: <AppstoreOutlined />,
+  },
+  {
+    key: 'sub3',
+    label: 'Người theo dõi',
+    // icon: <SettingOutlined />,
+  },
+  {
+    key: 'sub4',
+    label: 'Thư viện',
+  },
+];
 
 const Home: React.FC = () => {
     const [desti, setDesti] = useState<DestinationCardType[]>([]);
@@ -56,21 +111,36 @@ const Home: React.FC = () => {
         };
         fetchData();
     }, []);
-    console.log(userData)
 
     return (
-        <div className="flex mt-8 mx-[200px] gap-10">
+        <div className="flex mt-8 mx-[10%] gap-8">
             {/* Phần bên trái */}
-            <div className="w-1/4 bg-red-100 sticky top-[88px] h-full">
-                <div>
-                    <img className='rounded-full w-28 m-auto mt-8' src={userData?.profilePic.profilePicture} alt="avatar" />
-                    <p className='text-center mt-4 text-xl font-semibold'>{userData?.name}</p>
+            <div className="w-1/4 sticky top-[88px] h-screen rounded-lg overflow-hidden">
+                <div className='bg-gradient-to-b from-red-300 to-red-100 pt-8'>
+                    <img className='rounded-full w-28 m-auto' src={userData?.profilePic.profilePicture} alt="avatar" />
+                    <p className='text-center mt-4 text-xl font-semibold pb-2'>{userData?.name}</p>
+                </div>
+                <div className='grid grid-cols-2 grid-rows-2 bg-white p-4'>
+                    <div className='flex items-center gap-2 text-red-500'><p className='rounded-full bg-pink-100 w-10 h-10 flex items-center justify-center'><FaHandHoldingHeart /></p><div className='flex flex-col'><p>Cảm xúc</p><p className='text-black'>0</p></div></div>
+                    <div className='flex items-center gap-2 text-green-500'><p className='rounded-full bg-green-100 w-10 h-10 flex items-center justify-center'><BiSolidCommentDetail /></p><div className='flex flex-col'><p>Bình luận</p><p className='text-black'>0</p></div></div>
+                    <div className='flex items-center gap-2 text-blue-500 '><p className='rounded-full bg-blue-100 w-10 h-10 flex items-center justify-center'><BsFilePostFill /></p><div className='flex flex-col'><p>Bài đăng</p><p className='text-black'>0</p></div></div>
+                    <div className='flex items-center gap-2 text-purple-500 '><p className='rounded-full bg-purple-100 w-10 h-10 flex items-center justify-center'><RiUserFollowFill /></p><div className='flex flex-col'><p>Theo dõi</p><p className='text-black'>0</p></div></div>
+                </div>
+                <div className='mt-2'> 
+                    <Menu
+                        // onClick={onClick}
+                        style={{ width: '100%' }}
+                        defaultSelectedKeys={['1']}
+                        defaultOpenKeys={['sub1']}
+                        mode="inline"
+                        items={items}
+                    />
                 </div>
             </div>
 
             {/* Phần giữa */}
             <div className="flex-1 overflow-y-auto">
-                <div className="m-auto bg-white p-4 ">
+                <div className="m-auto bg-white p-4 rounded-lg">
                     <div className="flex items-center gap-2 pl-2 mb-2 text-lg font-semibold">
                         <p>Khám phá những địa điểm nổi bật </p>
                         <BsStars className="text-yellow-400" />
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ConfigProvider
+        theme={{
+          components: {
+            Menu: {
+              itemActiveBg: "#fee2e2",
+              itemSelectedBg: "#fee2e2",
+              itemSelectedColor: "red",
+            }
+          }
+        }}
+      >
+        <App />
+      </ConfigProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
