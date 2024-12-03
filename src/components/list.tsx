b890065f4c07409debe7142efd5997001b46a8f3
BaDuc1801 <minhduc180104@gmail.com>
2024-12-04 00:41:50 +0700

    xong post

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import { CommentProps } from './posts&comments/CommentCard.tsx';
-import { PostProps } from './posts&comments/PostCard.tsx';
+// import { PostProps } from './posts&comments/PostCard.tsx';
 import testImg from '../assets/img/home.jpg';
 
 const listcomment : CommentProps[] = [
